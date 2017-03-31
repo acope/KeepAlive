fa81591cb118a30a118ecf5c7dff272cf244aef8
Austin <copeman.austin@gmail.com>
2017-03-31 15:06:30 -0400

    Initial Commit

---
+++ keepComputerAlive.ps1
@@ -0,0 +1,14 @@
+﻿#Keeps the computer from falling asleep
+#Open notepad and writes a "." at specified interval and run at until specified time runs out
+
+Start-Process 'C:\windows\system32\notepad.exe' #opens notepad
+
+param($minutes = 240) #Number of minutes to keep the system alive
+$keypressDelay = 240 #keypress delay in seconds
+..
+$myshell = New-Object -com "Wscript.Shell"
+
+for ($i = 0; $i -lt $minutes; $i++) {
+  Start-Sleep -Seconds $keypressDelay
+  $myshell.sendkeys(".")
+}
