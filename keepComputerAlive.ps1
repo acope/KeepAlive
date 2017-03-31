8c56a1609eab9dd67c7913ab68d2a84468eee00f
Austin <copeman.austin@gmail.com>
2017-03-31 15:19:19 -0400

    Updates
    -Change variable keyDelay to keywait
    -Added keywait to param

--- keepComputerAlive.ps1
+++ keepComputerAlive.ps1
@@ -1,14 +1,12 @@
-﻿#Keeps the computer from falling asleep
+#Keeps the computer from falling asleep
 #Open notepad and writes a "." at specified interval and run at until specified time runs out
+param($minutes = 240, $keywait = 240) #Number of minutes to keep the system alive and key wait time in seconds
 
 Start-Process 'C:\windows\system32\notepad.exe' #opens notepad
 
-param($minutes = 240) #Number of minutes to keep the system alive
-$keypressDelay = 240 #keypress delay in seconds
-..
 $myshell = New-Object -com "Wscript.Shell"
 
 for ($i = 0; $i -lt $minutes; $i++) {
-  Start-Sleep -Seconds $keypressDelay
+  Start-Sleep -Seconds $keywait
   $myshell.sendkeys(".")
 }
