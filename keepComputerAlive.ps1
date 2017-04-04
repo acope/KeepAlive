b0a246f22d36ed9777f9e3712f20b8336d372a69
Austin <copeman.austin@gmail.com>
2017-04-04 11:07:40 -0400

    Major Update
    -Changed from For loop to While
    -Added user input at the end
    -Changed the wait
    -Changed input parameter to one. Default is 60 minutes

--- keepComputerAlive.ps1
+++ keepComputerAlive.ps1
@@ -1,12 +1,32 @@
 #Keeps the computer from falling asleep
 #Open notepad and writes a "." at specified interval and run at until specified time runs out
-param($minutes = 240, $keywait = 240) #Number of minutes to keep the system alive and key wait time in seconds
 
-Start-Process 'C:\windows\system32\notepad.exe' #opens notepad
+param($minutes = 60) #Number of minutes to keep the system alive, default is 60 minutes
 
+#$notepad = Start-Process 'C:\windows\system32\notepad.exe' -passthru #opens notepad
+$notepad = Start-Process notepad -PassThru #opens notepad
 $myshell = New-Object -com "Wscript.Shell"
+$timer = 60 * $minutes
 
-for ($i = 0; $i -lt $minutes; $i++) {
-  Start-Sleep -Seconds $keywait
-  $myshell.sendkeys(".")
+#Continue pressing the keypad while Notepad is still open or the timer has not run out.
+while(-not $notepad.HasExited -and ($timer -ge 0)){
+        Start-Sleep -Seconds 1 
+        Write-Host Seconds Remaining: $timer
+        $timer--
+        #Before keypress check to make sure Notepad has not been closed
+        if(-not $notepad.HasExited){
+            $myshell.sendkeys(".")
+        }
+}
+
+#User input is required to close console
+if($notepad.HasExited){
+    Stop-Process $notepad
+    Write-Host "Process has been terminated early"
+    Read-Host "Press ENTER key to exit..."
+    exit
+}else{
+    Write-Host "Timer has expired"
+    Read-Host "Press ENTER key to exit..."
+    exit
 }
